9efa384f31e00d9baef844a77a678f380b497773
Michal Rychlewicz <michalrychlewicz@gmail.com>
2019-10-26 17:52:20 +0200

    Applied small refactoring on ValueObject class

--- src/BuildingBlocks/Domain/ValueObject.cs
+++ src/BuildingBlocks/Domain/ValueObject.cs
@@ -7,8 +7,8 @@ namespace CompanyName.MyMeetings.BuildingBlocks.Domain
 {
     public abstract class ValueObject : IEquatable<ValueObject>
     {
-        private List<PropertyInfo> properties;
-        private List<FieldInfo> fields;
+        private List<PropertyInfo> _properties;
+        private List<FieldInfo> _fields;
 
         public static bool operator ==(ValueObject obj1, ValueObject obj2)
         {
@@ -53,9 +53,9 @@ namespace CompanyName.MyMeetings.BuildingBlocks.Domain
 
         private IEnumerable<PropertyInfo> GetProperties()
         {
-            if (this.properties == null)
+            if (this._properties == null)
             {
-                this.properties = GetType()
+                this._properties = GetType()
                     .GetProperties(BindingFlags.Instance | BindingFlags.Public)
                     .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
                     .ToList();
@@ -64,19 +64,19 @@ namespace CompanyName.MyMeetings.BuildingBlocks.Domain
                 // !Attribute.IsDefined(p, typeof(IgnoreMemberAttribute))).ToList();
             }
 
-            return this.properties;
+            return this._properties;
         }
 
         private IEnumerable<FieldInfo> GetFields()
         {
-            if (this.fields == null)
+            if (this._fields == null)
             {
-                this.fields = GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                this._fields = GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
                     .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
                     .ToList();
             }
 
-            return this.fields;
+            return this._fields;
         }
 
         public override int GetHashCode()
@@ -102,9 +102,7 @@ namespace CompanyName.MyMeetings.BuildingBlocks.Domain
 
         private int HashValue(int seed, object value)
         {
-            var currentHash = value != null
-                ? value.GetHashCode()
-                : 0;
+            var currentHash = value?.GetHashCode() ?? 0;
 
             return seed * 23 + currentHash;
         }
