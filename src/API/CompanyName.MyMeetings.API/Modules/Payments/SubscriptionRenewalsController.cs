5de0bbe281397209795d893c31dca25612fc50c2
Piotr Wachulec <p.wachulec@gmail.com>
2020-06-25 10:20:02 +0200

    Created endpoint for confirming renewal payments
    For test purposes

---
+++ src/API/CompanyName.MyMeetings.API/Modules/Payments/SubscriptionRenewalsController.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using CompanyName.MyMeetings.API.Configuration.Authorization;
+using CompanyName.MyMeetings.Modules.Payments.Application.Contracts;
+using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.MarkSubscriptionRenewalPaymentAsPaid;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyName.MyMeetings.API.Modules.Payments
+{
+    [Route("api/payments/subscriptionRenewals")]
+    [ApiController]
+    public class SubscriptionRenewalsController : ControllerBase
+    {
+        private readonly IPaymentsModule _paymentsModule;
+
+        public SubscriptionRenewalsController(IPaymentsModule paymentsModule)
+        {
+            _paymentsModule = paymentsModule;
+        }
+
+        [HttpPost]
+        [HasPermission(PaymentsPermissions.RegisterPayment)]
+        public async Task<IActionResult> RegisterSubscriptionPayment(RegisterSubscriptionRenewalPaymentRequest request)
+        {
+            await _paymentsModule.ExecuteCommandAsync(new MarkSubscriptionRenewalPaymentAsPaidCommand(request.PaymentId));
+
+            return Ok();
+        }
+    }
+}
