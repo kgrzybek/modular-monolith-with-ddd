31305cae897ef32f1a760ddff67f17cfe58ff339
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-05 13:23:11 +0200

    Added renewal in payments integration tests

--- src/Modules/Payments/Tests/IntegrationTests/Subscriptions/SubscriptionLifecycleTests.cs
+++ src/Modules/Payments/Tests/IntegrationTests/Subscriptions/SubscriptionLifecycleTests.cs
@@ -5,15 +5,15 @@ using System.Threading.Tasks;
 using CompanyName.MyMeetings.BuildingBlocks.IntegrationTests.Probing;
 using CompanyName.MyMeetings.Modules.Payments.Application.Contracts;
 using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.BuySubscription;
-using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.CreateSubscription;
-using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.ExpireSubscription;
+using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.BuySubscriptionRenewal;
 using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.ExpireSubscriptions;
 using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.GetSubscriptionDetails;
 using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.GetSubscriptionPayments;
 using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.MarkSubscriptionPaymentAsPaid;
-using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.RenewSubscription;
+using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.MarkSubscriptionRenewalPaymentAsPaid;
 using CompanyName.MyMeetings.Modules.Payments.Domain.MeetingPayments;
 using CompanyName.MyMeetings.Modules.Payments.Domain.SubscriptionPayments;
+using CompanyName.MyMeetings.Modules.Payments.Domain.SubscriptionRenewalPayments;
 using CompanyName.MyMeetings.Modules.Payments.Domain.Subscriptions;
 using CompanyName.MyMeetings.Modules.Payments.IntegrationTests.SeedWork;
 using NUnit.Framework;
@@ -26,7 +26,8 @@ namespace CompanyName.MyMeetings.Modules.Payments.IntegrationTests.Subscriptions
         [Test]
         public async Task Subscription_Buy_ThenRenew_ThenExpire_Test()
         {
-            SystemClock.Set(new DateTime(2020, 6, 15));
+            DateTime referenceDate = new DateTime(2020, 6, 15);
+            SystemClock.Set(referenceDate);
 
             var subscriptionPaymentId = await PaymentsModule.ExecuteCommandAsync(
                 new BuySubscriptionCommand(
@@ -40,26 +41,75 @@ namespace CompanyName.MyMeetings.Modules.Payments.IntegrationTests.Subscriptions
                     x => true), 
                 10000);
 
-            Assert.That(subscriptionPayments[0].Status, Is.EqualTo(SubscriptionPaymentStatus.WaitingForPayment.Code));
+            var subscriptionPayment = subscriptionPayments.Single(x => x.PaymentId == subscriptionPaymentId);
+
+            Assert.That(subscriptionPayment.Status, Is.EqualTo(SubscriptionPaymentStatus.WaitingForPayment.Code));
 
             await PaymentsModule.ExecuteCommandAsync(
                 new MarkSubscriptionPaymentAsPaidCommand(subscriptionPaymentId));
 
             subscriptionPayments = await GetEventually(
                 new GetSubscriptionPaymentsProbe(PaymentsModule, ExecutionContext.UserId,
-                    x => true),
+                    x => x.TrueForAll(payment => payment.SubscriptionId != null)),
                 10000);
+            
+            subscriptionPayment = subscriptionPayments.Single(x => x.PaymentId == subscriptionPaymentId);
+            
+            var subscriptionId = subscriptionPayment.SubscriptionId.GetValueOrDefault();
+            
+            var subscription = await GetEventually(
+                new GetSubscriptionDetailsProbe(
+                    PaymentsModule, 
+                    subscriptionId,
+                    x => x.SubscriptionId == subscriptionId &&
+                         x.Status == SubscriptionStatus.Active.Code && 
+                         x.Period == SubscriptionPeriod.Month.Code), 5000);
 
             Assert.That(subscriptionPayments[0].Status, Is.EqualTo(SubscriptionPaymentStatus.Paid.Code));
+            Assert.That(subscription.ExpirationDate, Is.EqualTo(referenceDate.AddMonths(1)));
+
+            var subscriptionRenewalPaymentId = await PaymentsModule.ExecuteCommandAsync(
+                new BuySubscriptionRenewalCommand(
+                    subscriptionId,
+                    "HalfYear",
+                    "PL",
+                    320,
+                    "PLN"));
 
             subscriptionPayments = await GetEventually(
                 new GetSubscriptionPaymentsProbe(PaymentsModule, ExecutionContext.UserId,
-                    x => x.Any(y => y.SubscriptionId.HasValue)),
+                    x => true), 
                 10000);
 
-            var subscriptionId = subscriptionPayments[0].SubscriptionId.Value;
+            var renewalPayment = subscriptionPayments
+                .Single(x => x.PaymentId == subscriptionRenewalPaymentId);
 
-            SystemClock.Set(new DateTime(2020, 7, 16));
+            Assert.That(renewalPayment.Status, Is.EqualTo(SubscriptionRenewalPaymentStatus.WaitingForPayment.Code));
+
+            await PaymentsModule.ExecuteCommandAsync(
+                new MarkSubscriptionRenewalPaymentAsPaidCommand(subscriptionRenewalPaymentId));
+
+            subscriptionPayments = await GetEventually(
+                new GetSubscriptionPaymentsProbe(PaymentsModule, ExecutionContext.UserId,
+                    x => true), 
+                10000);
+            
+            renewalPayment = subscriptionPayments
+                .Single(x => x.PaymentId == subscriptionRenewalPaymentId);
+
+            subscription = await GetEventually(
+                new GetSubscriptionDetailsProbe(
+                    PaymentsModule, 
+                    subscriptionId,
+                    x => x.SubscriptionId == subscriptionId &&
+                         x.Period == SubscriptionPeriod.GetName(SubscriptionPeriod.HalfYear.Code) &&
+                         x.Status == SubscriptionStatus.Active.Code
+                    ), 5000);
+            
+            Assert.That(renewalPayment.Status, Is.EqualTo(SubscriptionRenewalPaymentStatus.Paid.Code));
+            Assert.That(subscription.ExpirationDate, Is.EqualTo(referenceDate.AddMonths(7)));
+
+            SystemClock.Set(referenceDate.AddMonths(7).AddDays(1));
 
             await PaymentsModule.ExecuteCommandAsync(new ExpireSubscriptionsCommand());
 
@@ -128,13 +178,11 @@ namespace CompanyName.MyMeetings.Modules.Payments.IntegrationTests.Subscriptions
             }
         }
 
-        private class GetSubscriptionDetailsProbe : IProbe
+        private class GetSubscriptionDetailsProbe : IProbe<SubscriptionDetailsDto>
         {
             private readonly IPaymentsModule _paymentsModule;
 
             private readonly Guid _subscriptionId;
-
-            private SubscriptionDetailsDto _subscriptionDetailsDto;
 
             private readonly Func<SubscriptionDetailsDto, bool> _condition;
 
@@ -147,16 +195,15 @@ namespace CompanyName.MyMeetings.Modules.Payments.IntegrationTests.Subscriptions
                 _subscriptionId = subscriptionId;
                 _condition = condition;
             }
-
-            public bool IsSatisfied()
+            
+            public bool IsSatisfied(SubscriptionDetailsDto sample)
             {
-                return _subscriptionDetailsDto != null && _condition(_subscriptionDetailsDto);
+                return sample != null && _condition(sample);
             }
 
-            public async Task SampleAsync()
+            public async Task<SubscriptionDetailsDto> GetSampleAsync()
             {
-                _subscriptionDetailsDto =
-                    await _paymentsModule.ExecuteQueryAsync(new GetSubscriptionDetailsQuery(_subscriptionId));
+                return await _paymentsModule.ExecuteQueryAsync(new GetSubscriptionDetailsQuery(_subscriptionId));
             }
 
             public string DescribeFailureTo() => "Subscription read model is not in expected state";
