4cdd80023f0b840a5a45dc14ac39ca9291fb9ad1
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-05 13:03:11 +0200

    Added projections for new aggregate

--- src/Modules/Payments/Application/Subscriptions/GetSubscriptionPayments/SubscriptionPaymentsProjector.cs
+++ src/Modules/Payments/Application/Subscriptions/GetSubscriptionPayments/SubscriptionPaymentsProjector.cs
@@ -4,6 +4,7 @@ using CompanyName.MyMeetings.BuildingBlocks.Application.Data;
 using CompanyName.MyMeetings.BuildingBlocks.Domain;
 using CompanyName.MyMeetings.Modules.Payments.Application.Configuration.Projections;
 using CompanyName.MyMeetings.Modules.Payments.Domain.SubscriptionPayments.Events;
+using CompanyName.MyMeetings.Modules.Payments.Domain.SubscriptionRenewalPayments.Events;
 using CompanyName.MyMeetings.Modules.Payments.Domain.Subscriptions;
 using CompanyName.MyMeetings.Modules.Payments.Domain.Subscriptions.Events;
 using Dapper;
@@ -59,7 +60,43 @@ namespace CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.GetS
                 });
         }
 
-        private async Task When(SubscriptionPurchasedDomainEvent subscriptionPurchased)
+        private async Task When(SubscriptionRenewalPaymentCreatedDomainEvent subscriptionRenewalPaymentCreated)
+        {
+            string period = SubscriptionPeriod.GetName(subscriptionRenewalPaymentCreated.SubscriptionPeriodCode);
+
+            await _connection.ExecuteScalarAsync(
+                "INSERT INTO payments.SubscriptionPayments " +
+                "([PaymentId], [PayerId], [Type], [Status], [Period], [Date], " +
+                "[SubscriptionId], [MoneyValue], [MoneyCurrency]) " +
+                "VALUES (@SubscriptionRenewalPaymentId, @PayerId, @Type, @Status, @Period, " +
+                "@OccurredOn, @SubscriptionId, @Value, @Currency)",
+                new
+                {
+                    subscriptionRenewalPaymentCreated.SubscriptionRenewalPaymentId,
+                    subscriptionRenewalPaymentCreated.PayerId,
+                    Type = "Renewal Payment",
+                    subscriptionRenewalPaymentCreated.Status,
+                    period,
+                    subscriptionRenewalPaymentCreated.OccurredOn,
+                    subscriptionRenewalPaymentCreated.SubscriptionId,
+                    subscriptionRenewalPaymentCreated.Value,
+                    subscriptionRenewalPaymentCreated.Currency
+                });
+        }
+
+        private async Task When(SubscriptionRenewalPaymentPaidDomainEvent subscriptionRenewalPaymentPaid)
+        {
+            await _connection.ExecuteScalarAsync(
+                "UPDATE payments.SubscriptionPayments SET Status = @Status " +
+                "WHERE PaymentId = @SubscriptionRenewalPaymentId",
+                new
+                {
+                    subscriptionRenewalPaymentPaid.SubscriptionRenewalPaymentId,
+                    subscriptionRenewalPaymentPaid.Status
+                });
+        }
+
+        private async Task When(SubscriptionCreatedDomainEvent subscriptionCreated)
         {
             await _connection.ExecuteScalarAsync(
                 "UPDATE payments.SubscriptionPayments SET SubscriptionId = @SubscriptionId " +
