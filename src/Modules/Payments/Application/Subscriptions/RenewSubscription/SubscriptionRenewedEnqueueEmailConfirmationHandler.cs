7ee71b927064ee6edbd2c6b215c40ac5c7a5f716
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-17 11:39:38 +0200

    Extrated getting Payer email to provider class

--- src/Modules/Payments/Application/Subscriptions/RenewSubscription/SubscriptionRenewedEnqueueEmailConfirmationHandler.cs
+++ src/Modules/Payments/Application/Subscriptions/RenewSubscription/SubscriptionRenewedEnqueueEmailConfirmationHandler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using CompanyName.MyMeetings.BuildingBlocks.Application.Data;
 using CompanyName.MyMeetings.Modules.Payments.Application.Configuration.Commands;
 using CompanyName.MyMeetings.Modules.Payments.Application.Payers.GetPayer;
+using CompanyName.MyMeetings.Modules.Payments.Application.Payers.GetPayerEmail;
 using CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.SendSubscriptionRenewalConfirmationEmail;
 using CompanyName.MyMeetings.Modules.Payments.Domain.Subscriptions;
 using Dapper;
@@ -26,32 +27,14 @@ namespace CompanyName.MyMeetings.Modules.Payments.Application.Subscriptions.Rene
         
         public async Task Handle(SubscriptionRenewedNotification notification, CancellationToken cancellationToken)
         {
-            var payer = await GetPayerDetails(notification.DomainEvent.PayerId);
+            PayerEmailProvider.Init(_sqlConnectionFactory);
+            
+            var payer = await PayerEmailProvider.GetPayerDetails(notification.DomainEvent.PayerId);
             
             await _commandsScheduler.EnqueueAsync(new SendSubscriptionRenewalConfirmationEmailCommand(
                 Guid.NewGuid(),
                 new SubscriptionId(notification.DomainEvent.SubscriptionId),
                 payer.Email));
-        }
-        
-        private async Task<PayerDto> GetPayerDetails(Guid payerId)
-        {
-            var connection = _sqlConnectionFactory.GetOpenConnection();
-
-            const string sql = "SELECT " +
-                               "[Payer].[Id], " +
-                               "[Payer].[Login], " +
-                               "[Payer].[Email], " +
-                               "[Payer].[FirstName], " +
-                               "[Payer].[LastName], " +
-                               "[Payer].[Name] " +
-                               "FROM [payments].[Payers] AS [Payer] " +
-                               "WHERE [Payer].[Id] = @PayerId";
-
-            return await connection.QuerySingleAsync<PayerDto>(sql, new
-            {
-                payerId
-            });
         }
     }
 }
