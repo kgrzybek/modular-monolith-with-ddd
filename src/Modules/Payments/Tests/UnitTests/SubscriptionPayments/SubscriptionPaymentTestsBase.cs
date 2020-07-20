94d869d78bd46acb62324811d2534551a45e1a3a
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-20 09:50:28 +0200

    Created tests base for subscription payments

---
+++ src/Modules/Payments/Tests/UnitTests/SubscriptionPayments/SubscriptionPaymentTestsBase.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using CompanyName.MyMeetings.Modules.Payments.Domain.Payers;
+using CompanyName.MyMeetings.Modules.Payments.Domain.PriceListItems;
+using CompanyName.MyMeetings.Modules.Payments.Domain.SeedWork;
+using CompanyName.MyMeetings.Modules.Payments.Domain.Subscriptions;
+using CompanyName.MyMeetings.Modules.Payments.Domain.UnitTests.SeedWork;
+
+namespace CompanyName.MyMeetings.Modules.Payments.Domain.UnitTests.SubscriptionPayments
+{
+    public class SubscriptionPaymentTestsBase : TestBase
+    {
+        protected class SubscriptionPaymentTestData
+        {
+            public SubscriptionPaymentTestData(PriceList priceList, PayerId payerId, SubscriptionId subscriptionId)
+            {
+                PriceList = priceList;
+                PayerId = payerId;
+                SubscriptionId = subscriptionId;
+            }
+            
+            internal PriceList PriceList { get; }
+            
+            internal PayerId PayerId { get; }
+
+            internal SubscriptionId SubscriptionId { get; }
+        }
+
+        protected SubscriptionPaymentTestData CreateSubscriptionPaymentTestData()
+        {
+            var payerId = new PayerId(Guid.NewGuid());
+            var subscriptionId = new SubscriptionId(Guid.NewGuid());
+            var priceList = CreatePriceList();
+            
+            var subscriptionPaymentTestData = new SubscriptionPaymentTestData(
+                priceList,
+                payerId,
+                subscriptionId);
+
+            return subscriptionPaymentTestData;
+        }
+        
+        private PriceList CreatePriceList()
+        {
+            var priceListItem = new PriceListItemData(
+                "PL",
+                SubscriptionPeriod.Month, 
+                MoneyValue.Of(60, "PLN"),
+                PriceListItemCategory.New
+            );
+            
+            var priceList = new PriceList(new List<PriceListItemData>{ priceListItem });
+
+            return priceList;
+        }
+    }
+}
