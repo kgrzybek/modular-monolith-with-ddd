aaf3d11b2cee6c361316838a985f5817690502e4
Andrei Ganichev <aganichev10@gmail.com>
2020-07-23 23:35:28 +0300

    Commenting. Add Meeting, MeetingComments tests.

---
+++ src/Modules/Meetings/Tests/UnitTests/MeetingComments/MeetingCommentTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.Comments.Events;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.MeetingComments;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.MeetingComments.Rules;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.Meetings;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.Members;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.UnitTests.SeedWork;
+using NUnit.Framework;
+
+namespace CompanyName.MyMeetings.Modules.Meetings.Domain.UnitTests.MeetingComments
+{
+    [TestFixture]
+    public class MeetingCommentTests : TestBase
+    {
+        [Test]
+        public void CreateMeetingComment_IsSuccessful()
+        {
+            var meetingComment = MeetingComment.Create(
+                new MeetingId(Guid.NewGuid()),
+                new MemberId(Guid.NewGuid()),
+                "Great meeting!");
+            
+            var meetingCommentCreated = AssertPublishedDomainEvent<MeetingCommentCreatedDomainEvent>(meetingComment);
+
+            Assert.That(meetingCommentCreated.MeetingCommentId, Is.EqualTo(meetingComment.Id));
+        }
+        
+        [Test]
+        public void CreateMeetingComment_BreaksCommentTextMustBeProvidedRule()
+        {
+            AssertBrokenRule<CommentTextMustBeProvidedRule>(() =>
+            {
+                MeetingComment.Create(
+                    new MeetingId(Guid.NewGuid()),
+                    new MemberId(Guid.NewGuid()),
+                    comment: null);
+            });
+        }
+    }
+}
---
+++ src/Modules/Meetings/Tests/UnitTests/Meetings/MeetingAddCommentTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.Meetings.Rules;
+using CompanyName.MyMeetings.Modules.Meetings.Domain.Members;
+using NUnit.Framework;
+
+namespace CompanyName.MyMeetings.Modules.Meetings.Domain.UnitTests.Meetings
+{
+    [TestFixture]
+    public class MeetingAddCommentTests : MeetingTestsBase
+    {
+        [Test]
+        public void AddComment_BreaksCommentCanBeAddedOnlyByAttendeeRule()
+        {
+            var meeting = CreateMeetingTestData(new MeetingTestDataOptions()).Meeting;
+
+            AssertBrokenRule<CommentCanBeAddedOnlyByAttendeeRule>(() =>
+            {
+                meeting.AddComment(new MemberId(Guid.NewGuid()), "Great meeting!");
+            });
+        }
+    }
+}
