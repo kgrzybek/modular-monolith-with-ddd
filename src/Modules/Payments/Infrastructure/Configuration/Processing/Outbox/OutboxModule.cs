3870da3a9331b9ec99979982b244887a8c3cc222
Kamil Grzybek <kamgrzybek@gmail.com>
2020-07-30 00:07:35 +0200

    Add mapping between Domain Event Notifications CLRs and names check during startup

--- src/Modules/Payments/Infrastructure/Configuration/Processing/Outbox/OutboxModule.cs
+++ src/Modules/Payments/Infrastructure/Configuration/Processing/Outbox/OutboxModule.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Autofac;
+using CompanyName.MyMeetings.BuildingBlocks.Application.Events;
 using CompanyName.MyMeetings.BuildingBlocks.Application.Outbox;
 using CompanyName.MyMeetings.BuildingBlocks.Infrastructure;
 using CompanyName.MyMeetings.BuildingBlocks.Infrastructure.DomainEventsDispatching;
@@ -23,11 +26,37 @@ namespace CompanyName.MyMeetings.Modules.Payments.Infrastructure.Configuration.P
                 .FindConstructorsWith(new AllConstructorFinder())
                 .InstancePerLifetimeScope();
 
+            this.CheckMappings();
+
             builder.RegisterType<DomainNotificationsMapper>()
                 .As<IDomainNotificationsMapper>()
                 .FindConstructorsWith(new AllConstructorFinder())
                 .WithParameter("domainNotificationsMap", _domainNotificationsMap)
                 .SingleInstance();
+        }
+
+        private void CheckMappings()
+        {
+            var domainEventNotifications = Assemblies.Application
+                .GetTypes()
+                .Where(x => x.GetInterfaces().Contains(typeof(IDomainEventNotification)))
+                .ToList();
+
+            List<Type> notMappedNotifications = new List<Type>();
+            foreach (var domainEventNotification in domainEventNotifications)
+            {
+                _domainNotificationsMap.TryGetBySecond(domainEventNotification, out var name);
+
+                if (name == null)
+                {
+                    notMappedNotifications.Add(domainEventNotification);
+                }
+            }
+
+            if (notMappedNotifications.Any())
+            {
+                throw new ApplicationException($"Domain Event Notifications {notMappedNotifications.Select(x => x.FullName).Aggregate((x, y) => x + "," + y)} not mapped");
+            }
         }
     }
 }
