dcb1f5dcd0f0fddf8077bcdcb28abc2052da4198
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-29 18:24:49 +0200

    Payments tests (#91)
    * Created base class for subscription renewal payments tests
    
    * Created tests for subscription renewal payments
    
    * Added test for checking breaking rule in subscription renewal payment
    
    * Removed unused class
    
    * Created tests base for subscription payments
    
    * Created subscription payments tests
    
    * Created Subscription aggregate unit tests
    
    * Updated PriceList creation in tests bases

---
+++ src/Modules/Payments/Tests/UnitTests/SubscriptionRenewalPayments/SubscriptionRenewalPaymentTests.cs
@@ -0,0 +1,75 @@
+using CompanyName.MyMeetings.Modules.Payments.Domain.SeedWork;
+using CompanyName.MyMeetings.Modules.Payments.Domain.SubscriptionRenewalPayments;
+using CompanyName.MyMeetings.Modules.Payments.Domain.SubscriptionRenewalPayments.Events;
+using CompanyName.MyMeetings.Modules.Payments.Domain.SubscriptionRenewalPayments.Rules;
+using CompanyName.MyMeetings.Modules.Payments.Domain.Subscriptions;
+using NUnit.Framework;
+
+namespace CompanyName.MyMeetings.Modules.Payments.Domain.UnitTests.SubscriptionRenewalPayments
+{
+    [TestFixture]
+    public class SubscriptionRenewalPaymentTests : SubscriptionRenewalPaymentTestsBase
+    {
+        [Test]
+        public void BuySubscriptionRenewal_IsSuccessful()
+        {
+            // Arrange
+            var subscriptionRenewalPaymentTestData = CreateSubscriptionRenewalPaymentTestData();
+
+            // Act
+            var subscriptionRenewalPayment = SubscriptionRenewalPayment.Buy(
+                subscriptionRenewalPaymentTestData.PayerId,
+                subscriptionRenewalPaymentTestData.SubscriptionId, 
+                SubscriptionPeriod.Month,
+                "PL",
+                MoneyValue.Of(60, "PLN"),
+                subscriptionRenewalPaymentTestData.PriceList
+            );
+            
+            // Assert
+            AssertPublishedDomainEvent<SubscriptionRenewalPaymentCreatedDomainEvent>(subscriptionRenewalPayment);
+        }
+
+        [Test]
+        public void BuySubscriptionRenewal_WhenPriceDoesNotExist_IsNotPossible()
+        {
+            // Arrange
+            var subscriptionRenewalPaymentTestData = CreateSubscriptionRenewalPaymentTestData();
+
+            // Act & Assert
+            AssertBrokenRule<PriceOfferMustMatchPriceInPriceListRule>(() =>
+            {
+                SubscriptionRenewalPayment.Buy(
+                    subscriptionRenewalPaymentTestData.PayerId,
+                    subscriptionRenewalPaymentTestData.SubscriptionId, 
+                    SubscriptionPeriod.Month,
+                    "PL",
+                    MoneyValue.Of(50, "PLN"),
+                    subscriptionRenewalPaymentTestData.PriceList
+                );
+            });
+        }
+
+        [Test]
+        public void PaySubscriptionRenewal_IsSuccessful()
+        {
+            // Arrange
+            var subscriptionRenewalPaymentTestData = CreateSubscriptionRenewalPaymentTestData();
+
+            var subscriptionRenewalPayment = SubscriptionRenewalPayment.Buy(
+                subscriptionRenewalPaymentTestData.PayerId,
+                subscriptionRenewalPaymentTestData.SubscriptionId, 
+                SubscriptionPeriod.Month,
+                "PL",
+                MoneyValue.Of(60, "PLN"),
+                subscriptionRenewalPaymentTestData.PriceList
+            );
+            
+            // Act
+            subscriptionRenewalPayment.MarkRenewalAsPaid();
+            
+            // Assert
+            AssertPublishedDomainEvent<SubscriptionRenewalPaymentPaidDomainEvent>(subscriptionRenewalPayment);
+        }
+    }
+}
---
+++ src/Modules/Payments/Tests/UnitTests/SubscriptionRenewalPayments/SubscriptionRenewalPaymentTestsBase.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using CompanyName.MyMeetings.Modules.Payments.Domain.Payers;
+using CompanyName.MyMeetings.Modules.Payments.Domain.PriceListItems;
+using CompanyName.MyMeetings.Modules.Payments.Domain.SeedWork;
+using CompanyName.MyMeetings.Modules.Payments.Domain.Subscriptions;
+using CompanyName.MyMeetings.Modules.Payments.Domain.UnitTests.SeedWork;
+
+namespace CompanyName.MyMeetings.Modules.Payments.Domain.UnitTests.SubscriptionRenewalPayments
+{
+    public class SubscriptionRenewalPaymentTestsBase : TestBase
+    {
+        protected class SubscriptionRenewalPaymentTestData
+        {
+            public SubscriptionRenewalPaymentTestData(PriceList priceList, PayerId payerId, SubscriptionId subscriptionId)
+            {
+                PriceList = priceList;
+                PayerId = payerId;
+                SubscriptionId = subscriptionId;
+            }
+            
+            internal PriceList PriceList { get; }
+            
+            internal PayerId PayerId { get; }
+
+            internal SubscriptionId SubscriptionId { get; }
+        }
+
+        protected SubscriptionRenewalPaymentTestData CreateSubscriptionRenewalPaymentTestData()
+        {
+            var payerId = new PayerId(Guid.NewGuid());
+            var subscriptionId = new SubscriptionId(Guid.NewGuid());
+            var priceList = CreatePriceList();
+            
+            var subscriptionRenewalPaymentTestData = new SubscriptionRenewalPaymentTestData(
+                priceList,
+                payerId,
+                subscriptionId);
+
+            return subscriptionRenewalPaymentTestData;
+        }
+        
+        private PriceList CreatePriceList()
+        {
+            var priceListItem = new PriceListItemData(
+                "PL",
+                SubscriptionPeriod.Month, 
+                MoneyValue.Of(60, "PLN"),
+                PriceListItemCategory.Renewal
+            );
+            
+            var priceList = PriceList.CreateFromItems(new List<PriceListItemData>{ priceListItem });
+
+            return priceList;
+        }
+    }
+}
