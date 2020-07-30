dd097ff4def2546b385b45e2531b72ef09e33bff
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-30 10:00:50 +0200

    Removed redundant this word and code cleanup

--- src/API/CompanyName.MyMeetings.API/Configuration/ExecutionContext/CorrelationMiddleware.cs
+++ src/API/CompanyName.MyMeetings.API/Configuration/ExecutionContext/CorrelationMiddleware.cs
@@ -9,22 +9,18 @@ namespace CompanyName.MyMeetings.API.Configuration.ExecutionContext
         internal const string CorrelationHeaderKey = "CorrelationId";
         private readonly RequestDelegate _next;
 
-        public CorrelationMiddleware(
-            RequestDelegate next)
+        public CorrelationMiddleware(RequestDelegate next)
         {
-            this._next = next;
+            _next = next;
         }
 
         public async Task Invoke(HttpContext context)
         {
             var correlationId = Guid.NewGuid();
 
-            if (context.Request != null)
-            {
-                context.Request.Headers.Add(CorrelationHeaderKey, correlationId.ToString());
-            }
+            context.Request?.Headers.Add(CorrelationHeaderKey, correlationId.ToString());
 
-            await this._next.Invoke(context);
+            await _next.Invoke(context);
         }
     }
 }
