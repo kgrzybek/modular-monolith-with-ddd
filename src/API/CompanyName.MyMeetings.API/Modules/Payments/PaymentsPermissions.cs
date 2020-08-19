1e81177c34bcf778001d8782646cb0ad0e2cd64d
Piotr Wachulec <p.wachulec@gmail.com>
2020-08-19 09:07:43 +0200

    Removed unnecessary empty lines

--- src/API/CompanyName.MyMeetings.API/Modules/Payments/PaymentsPermissions.cs
+++ src/API/CompanyName.MyMeetings.API/Modules/Payments/PaymentsPermissions.cs
@@ -3,17 +3,11 @@
     public class PaymentsPermissions
     {
         public const string RegisterPayment = "RegisterPayment";
-
         public const string BuySubscription = "BuySubscription";
-
         public const string RenewSubscription = "RenewSubscription";
-
         public const string CreatePriceListItem = "CreatePriceListItem";
-
         public const string ActivatePriceListItem = "ActivatePriceListItem";
-
         public const string DeactivatePriceListItem = "DeactivatePriceListItem";
-
         public const string ChangePriceListItemAttributes = "ChangePriceListItemAttributes";
     }
 }
