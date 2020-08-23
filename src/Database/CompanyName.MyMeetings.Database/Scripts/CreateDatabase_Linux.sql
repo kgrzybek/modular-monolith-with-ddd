673557a65756ec40394796aaf8bd012faaa6718d
Kamil Grzybek <kamgrzybek@gmail.com>
2020-08-23 22:25:46 +0200

    Add Database Change Management description in README. Update How To Run section.

--- src/Database/CompanyName.MyMeetings.Database/Scripts/CreateDatabase_Linux.sql
+++ src/Database/CompanyName.MyMeetings.Database/Scripts/CreateDatabase_Linux.sql
@@ -1,6 +1,6 @@
 ﻿CREATE DATABASE [MyMeetings]
  CONTAINMENT = NONE
- ON  PRIMARY 
+ ON PRIMARY 
 ( NAME = N'MyMeetings', FILENAME = N'/var/opt/mssql/data/MyMeetings.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
  LOG ON 
 ( NAME = N'MyMeetings_log', FILENAME = N'/var/opt/mssql/data/MyMeetings_log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
