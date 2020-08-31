51f60d99f50c6f17896cbd8ac5b7b4f4f72312b0
Andrei Ganichev <aganichev10@gmail.com>
2020-08-31 14:16:46 +0300

    Add database migration and add Integration Tests execution for Meetings module to CI pipeline. (#110)
    * Commenting. Add migration for change EditDate type and add MeetingComments view.
    
    * Cleanup. Fix warnings.
    
    * CI. Add Integration Tests execution for Meetings module to CI pipeline.

---
+++ src/Database/CompanyName.MyMeetings.Database/Scripts/Migrations/1_0_0_0/0002_change_meeting_comments_edit_date_type_and_add_meeting_comments_view.sql
@@ -0,0 +1,17 @@
+﻿ALTER TABLE [meetings].[MeetingComments] 
+ALTER COLUMN [EditDate] DATETIME 
+GO
+
+CREATE VIEW [meetings].[v_MeetingComments]
+AS
+SELECT
+    [MeetingComments].Id,
+    [MeetingComments].MeetingId,
+    [MeetingComments].AuthorId,
+    [MeetingComments].InReplyToCommentId,
+    [MeetingComments].Comment,
+    [MeetingComments].CreateDate,
+    [MeetingComments].EditDate
+FROM [meetings].[MeetingComments] AS [MeetingComments]
+WHERE [MeetingComments].IsRemoved = 0
+GO
