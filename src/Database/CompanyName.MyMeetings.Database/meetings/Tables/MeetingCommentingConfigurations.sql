6334cf14385a975d6fe28bd0e8abc59f661213f5
Andrei Ganichev <aganichev10@gmail.com>
2020-09-19 21:42:40 +0300

    Commenting. Add semicolon in script creating MeetingCommentingConfiguration.

--- src/Database/CompanyName.MyMeetings.Database/meetings/Tables/MeetingCommentingConfigurations.sql
+++ src/Database/CompanyName.MyMeetings.Database/meetings/Tables/MeetingCommentingConfigurations.sql
@@ -5,5 +5,5 @@
     [IsCommentingEnabled]   BIT              NOT NULL,
     CONSTRAINT [PK_meetings_MeetingCommentingConfigurations_Id] PRIMARY KEY ([Id] ASC),
     CONSTRAINT [FK_meetings_MeetingCommentingConfigurations_Meetings] FOREIGN KEY ([MeetingId]) REFERENCES meetings.Meetings ([Id])
-)
+);
 GO
