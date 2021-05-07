7e3026df97a2f8d99b58babf294087b19dba7ec9
Grégoire Wulliamoz <gwulliamoz@greenmotion.ch>
2021-05-07 06:56:48 +0200

    feat: remove useless QueryBase without result

--- src/Modules/Administration/Application/Configuration/Queries/QueryBase.cs
+++ src/Modules/Administration/Application/Configuration/Queries/QueryBase.cs
@@ -3,21 +3,6 @@ using CompanyName.MyMeetings.Modules.Administration.Application.Contracts;
 
 namespace CompanyName.MyMeetings.Modules.Administration.Application.Configuration.Queries
 {
-    public abstract class QueryBase
-    {
-        protected QueryBase()
-        {
-            this.Id = Guid.NewGuid();
-        }
-
-        protected QueryBase(Guid id)
-        {
-            this.Id = id;
-        }
-
-        public Guid Id { get; }
-    }
-
     public abstract class QueryBase<TResult> : IQuery<TResult>
     {
         protected QueryBase()
