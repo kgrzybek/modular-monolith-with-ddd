3585181dc77c137b98ad77161779dda86db77192
Bela Istok <bistok@bistok.net>
2023-11-29 22:36:39 -0500

    removed hardcoded paths this allows to use any SQL Version

--- src/Database/CompanyName.MyMeetings.Database/Scripts/CreateDatabase_Linux.sql
+++ src/Database/CompanyName.MyMeetings.Database/Scripts/CreateDatabase_Linux.sql
@@ -1,7 +1,5 @@
-﻿CREATE DATABASE [MyMeetings] CONTAINMENT = NONE 
-ON PRIMARY 
-( NAME = N'MyMeetings', FILENAME = N'/var/opt/mssql/data/MyMeetings.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB ) 
-LOG ON  ( NAME = N'MyMeetings_log', FILENAME = N'/var/opt/mssql/data/MyMeetings_log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+﻿CREATE DATABASE [MyMeetings] 
+CONTAINMENT = NONE 
 GO
 
 USE [MyMeetings]
