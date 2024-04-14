b2b65e9a5a29ba5e667586baaa849de2673eb2c1
Jeremiah Flaga <108422823+jflaga@users.noreply.github.com>
2024-04-15 01:54:05 +0800

    Add missing tables for registrations (#315)
    * fix: add missing tables for registrations
    
     - registrations.OutboxMessages
     - registrations.InboxMessages
     - registrations.InternalCommands
    
    * Add missing tables for registrations in migrations directory

---
+++ src/Database/CompanyName.MyMeetings.Database/Scripts/Migrations/1_0_0_0/0014_add_missing_tables_for_registrations.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE [registrations].[OutboxMessages]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[OccurredOn] DATETIME2 NOT NULL,
+	[Type] VARCHAR(255) NOT NULL,
+	[Data] VARCHAR(MAX) NOT NULL,
+	[ProcessedDate] DATETIME2 NULL,
+	CONSTRAINT [PK_registrations_OutboxMessages_Id] PRIMARY KEY ([Id] ASC)
+)
+GO
+
+CREATE TABLE [registrations].[InternalCommands]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[EnqueueDate] DATETIME2 NOT NULL,
+	[Type] VARCHAR(255) NOT NULL,
+	[Data] VARCHAR(MAX) NOT NULL,
+	[ProcessedDate] DATETIME2 NULL,
+	[Error] NVARCHAR(MAX) NULL,
+	CONSTRAINT [PK_registrations_InternalCommands_Id] PRIMARY KEY ([Id] ASC)
+)
+GO
+
+CREATE TABLE [registrations].[InboxMessages]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[OccurredOn] DATETIME2 NOT NULL,
+	[Type] VARCHAR(255) NOT NULL,
+	[Data] VARCHAR(MAX) NOT NULL,
+	[ProcessedDate] DATETIME2 NULL,
+	CONSTRAINT [PK_registrations_InboxMessages_Id] PRIMARY KEY ([Id] ASC)
+)
+GO
---
+++ src/Database/CompanyName.MyMeetings.Database/Structure/registrations/Tables/InternalCommands.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [registrations].InternalCommands
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[EnqueueDate] DATETIME2 NOT NULL,
+	[Type] VARCHAR(255) NOT NULL,
+	[Data] VARCHAR(MAX) NOT NULL,
+	[ProcessedDate] DATETIME2 NULL,
+	[Error] NVARCHAR(MAX) NULL,
+	CONSTRAINT [PK_registrations_InternalCommands_Id] PRIMARY KEY ([Id] ASC)
+)
+GO
